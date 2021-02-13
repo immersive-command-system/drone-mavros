c232606a96eb14bc0cee659355c5b49bbbabd658
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-02-13 13:16:08 -0800

    Added Topic/Service Remapping

--- src/server_connector/src/connect_to_server.py
+++ src/server_connector/src/connect_to_server.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
 import roslibpy
 import rospy
+import os
+import roslaunch
 
 rospy.init_node('server_connector')
 
 
-def register_drone(client):
+def register_drone(client, drone_name):
     service = roslibpy.Service(client, '/isaacs_server/register_drone', 'isaacs_server/register_drone')
-    request = roslibpy.ServiceRequest({'drone_name': 'hexacopter', 'drone_type': 'Mavros'})
+    request = roslibpy.ServiceRequest({'drone_name': drone_name, 'drone_type': 'Mavros'})
 
     result = service.call(request)
     rospy.loginfo(result)
     return result
 
+
+def launch_mavros(connection_info):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    new_path = os.path.join(dir_path, '../../../launch/drone.launch')
+    rospy.loginfo(new_path)
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+    roslaunch_file = [(new_path,
+                       ['namespace:=drone_' + str(connection_info['id'])])]
+    parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
+    parent.start()
+
+def publish_topics(server_connection, id):
+    service = roslibpy.Service(server_connection, '/isaacs_server/save_drone_topics', 'isaacs_server/type_to_topic')
+    request = roslibpy.ServiceRequest({"publishes": [{"name":"/drone_" + str(id) + "/mavros/set_mode",
+                                                      "type": "mavros_msgs/SetMode"}], "id": id})
+    result = service.call(request)
+    rospy.loginfo(result)
+    return result
 
 rospy.loginfo('Starting!')
 
@@ -23,7 +44,9 @@ server_connection.run(timeout=10)
 rospy.loginfo('Attempting to connect to: ' + server_ip)
 result = None
 if server_connection.is_connected:
-    result = register_drone(server_connection)
-    rospy.loginfo('Connected!')
+    result = register_drone(server_connection, 'hexacopter1')
+    rospy.loginfo('Connected! Launching MAVRos!')
+    launch_mavros(result)
+    publish_topics(server_connection, result['id'])
 
 rospy.spin()
