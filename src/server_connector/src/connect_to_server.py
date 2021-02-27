4ec1b79d34583a46c5a958e15c33d6ee7b9517f2
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-02-26 17:24:53 -0800

    Started topic_publisher

--- src/server_connector/src/connect_to_server.py
+++ src/server_connector/src/connect_to_server.py
@@ -3,7 +3,7 @@ import roslibpy
 import rospy
 import os
 import roslaunch
-import topic_publisher
+from topic_publisher import TopicPublisher
 
 rospy.init_node('server_connector')
 namespace = '/'
@@ -19,7 +19,8 @@ def register_drone(client, drone_name):
 
 def launch_mavros():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    new_path = os.path.join(dir_path, '../../../launch/drone.launch')
+    print(dir_path)
+    new_path = os.path.join(dir_path, '../launch/drone.launch')
     rospy.loginfo(new_path)
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
@@ -27,14 +28,6 @@ def launch_mavros():
                        ['namespace:=' + namespace])]
     parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
     parent.start()
-
-def publish_topics(server_connection, id):
-    service = roslibpy.Service(server_connection, '/isaacs_server/save_drone_topics', 'isaacs_server/type_to_topic')
-    request = roslibpy.ServiceRequest({"publishes": [{"name":namespace + "/mavros/set_mode",
-                                                      "type": "mavros_msgs/SetMode"}], "id": id})
-    result = service.call(request)
-    rospy.loginfo(result)
-    return result
 
 rospy.loginfo('Starting!')
 
@@ -45,11 +38,11 @@ server_connection.run(timeout=10)
 rospy.loginfo('Attempting to connect to: ' + server_ip)
 result = None
 if server_connection.is_connected:
-    result = register_drone(server_connection, 'hexacopter1')
-    namespace = "/drone_" + str(id)
+    result = register_drone(server_connection, 'hexacopter2')
+    namespace = "/drone_" + str(result['id'])
     rospy.loginfo('Connected! Launching MAVRos!')
     launch_mavros()
-    publish_topics(server_connection, result['id'])
+    topic_publisher = TopicPublisher(namespace, server_connection)
 
 r = rospy.Rate(10)
 rospy.loginfo(topic_publisher.get_topics(namespace))
--- src/server_connector/src/topic_publisher.py
+++ src/server_connector/src/topic_publisher.py
@@ -1,8 +1,58 @@
 #!/usr/bin/env python
 import rospy
+import roslibpy
+from mavros_msgs.srv import SetMode
 
+class TopicPublisher:
+    topic_to_type = {
+        'set_mode': 'mavros_msgs/SetMode',
+        'cmd/arming': 'mavros_msgs/CommandBool',
+        'cmd/takeoff': 'mavros_msgs/CommandTOL',
+        'mission/push': 'mavros_msgs/WaypointPush'
+    }
 
-def get_topics(namespace):
-    return rospy.get_published_topics(namespace)
+    service_name_exceptions = {
+        'arming': 'cmd/arming',
+        'takeoff': 'cmd/takeoff',
+        'push': 'mission/push'
+    }
+
+    def __init__(self, namespace, connection):
+        self.namespace = namespace
+        self.connection = connection
+        self.services = []
+
+    def get_topics(self):
+        return rospy.get_published_topics(self.namespace)
+
+    def global_service(self, handler):
+        """
+        This method is designed to be used as a decorator (@global_service)
+        to advertise the handler method as a service via the ROS_master_connection.
+        By default, the service can be found at `/mavros/[handler_name]`
+        with a service type found in the topic_to_type dictionary.
+
+        Exceptions for the handler name to service type mapping can be added
+        to the exceptions dictionary.None
+
+        parameter: handler(request, response) handles an incoming service request.
+        returns: handler
+        """
+        service_name = self.service_name_exceptions.get(handler.__name__, handler.__name__)
+        service = roslibpy.Service(self.connection, self.namespace +
+                                   '/mavros/{}'.format(service_name), self.topic_to_type[service_name])
+        print(service.name)
+        service.advertise(handler)
+        self.services.append(service)
+        return handler
+
+    @global_service
+    def set_mode(self, request, response):
+        set_mode_service = rospy.ServiceProxy(self.namespace + '/mavros/set_mode', SetMode)
+        set_mode_object = SetMode()
+        set_mode_object.custom_mode = request.get('custom_mode')
+        local_response = set_mode_service(set_mode_object)
+        response['mode_sent'] = local_response.mode_sent
+        return local_response.mode_sent
 
 
