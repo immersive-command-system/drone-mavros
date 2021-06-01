e2fb7e3ce63ffecd762555b31c4537814a2d2746
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-06-01 15:40:45 -0700

    Separate process method

--- src/depth_camera_publisher/src/depth_camera_publisher/sensor_registerer.py
+++ src/depth_camera_publisher/src/depth_camera_publisher/sensor_registerer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from server_connector.connection_manager import ConnectionManager
-from server_connector.connect_to_server import get_connection_manager
+from server_connector.srv import PublishTopic
 from sensor_msgs.msg import PointCloud2
 import roslibpy
 
@@ -22,13 +21,10 @@ def register_camera(server_connection):
                                        'sensor_type': 'depth_camera',
                                        'parent_drone_name': rospy.get_param('name', 'hexacopter')})
 
-connection_manager = ConnectionManager.instance
-print('Getting connection manager instance')
-print(connection_manager)
-print(get_connection_manager())
-if connection_manager != None:
+if __name__ == '__main__':
+    service = rospy.ServiceProxy('publish_topic', PublishTopic)
+    response = service('/camera/depth/points', 'sensor_msgs/PointCloud2', 'sensor_msgs.msg', 'PointCloud2',
+                       'depth_camera_publisher.sensor_registerer', 'point_cloud_publisher', True)
     connection_manager.topic_publisher.publish_topic('/camera/depth/points',
                                                      'sensor_msgs/PointCloud2', PointCloud2,
                                                      point_cloud_publisher, include_namespace=True)
-    server_connection = connection_manager.server_connection
-    register_camera(server_connection)
--- src/server_connector/src/server_connector/topic_publisher.py
+++ src/server_connector/src/server_connector/topic_publisher.py
@@ -9,8 +9,10 @@ from sensor_msgs.msg import NavSatFix
 from mavros_msgs.srv import CommandBool
 from mavros_msgs.srv import CommandTOL
 from mavros_msgs.srv import WaypointClear
-from server_connector.srv import AdvertiseService,AdvertiseServiceResponse
+from server_connector.srv import AdvertiseService, AdvertiseServiceResponse
+from server_connector.srv import PublishTopic, PublishTopicResponse
 from std_srvs.srv import Trigger
+
 
 class TopicPublisher:
 
@@ -22,6 +24,7 @@ class TopicPublisher:
         self.location = 0
         self.advertise_basic_services_topics()
         self.create_advertiser_service()
+        self.create_publisher_service()
 
     def advertise_basic_services_topics(self):
         self.advertise_service('/mavros/set_mode', 'mavros_msgs/SetMode', self.set_mode, include_namespace=True)
@@ -42,12 +45,25 @@ class TopicPublisher:
 
     def create_advertiser_service(self):
         def create_advertiser_service_handler(request):
-            function = importlib.import_module(request.handler_import)
-            self.advertise_service(request.service_name, request.service_type, function,
-                                   request.include_namespace)
+            function_module = importlib.import_module(request.publish_function_import_module)
+            function = getattr(function_module, request.publish_function_name)
+            self.publish_topic(request.topic_name, request.topic_type, function,
+                               request.include_namespace)
             return AdvertiseServiceResponse(True)
-        print('starting!')
+
         rospy.Service('advertise_service', AdvertiseService, create_advertiser_service_handler)
+
+    def create_publisher_service(self):
+        def create_publisher_service_handler(request):
+            function_module = importlib.import_module(request.publish_function_import_module)
+            function = getattr(function_module, request.publish_function_name)
+            message_type_module = importlib.import_module(request.topic_type_class_import_module)
+            message_type = getattr(message_type_module, request.topic_type_class_name)
+            self.publish_topic(request.topic_name, request.topic_type, message_type,
+                               function, request.include_namespace)
+            return PublishTopicResponse(True)
+
+        rospy.Service('publish_topic', PublishTopic, create_publisher_service_handler)
 
     def get_topics(self):
         return rospy.get_published_topics(self.namespace)
